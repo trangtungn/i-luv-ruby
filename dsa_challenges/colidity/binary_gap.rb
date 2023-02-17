ac437a29d7192a9186ea469cd96ccbb0079f973d
Trang Tung Nguyen <trangtungn@gmail.com>
2023-02-17 12:09:37 +0700

    Update binary gap

---
+++ dsa_challenges/colidity/binary_gap.rb
@@ -0,0 +1,50 @@
+#!/Users/trangtungn/.rbenv/shims/ruby -w
+
+def solution(n)
+  longest = 0
+  gap = 0
+
+  binary = n.to_s(2)
+
+  p binary
+
+  binary.each_char do |char|
+    if char == '1'
+      longest = gap if gap > longest
+
+      gap = 0
+    else
+      gap += 1
+    end
+  end
+
+  longest
+end
+
+# 7
+# 6
+# 6172
+# 9
+# 529
+# 20
+# 15
+# 32
+# 1041
+# 4
+p solution(1)
+p '-' * 10
+p solution(5)
+p '-' * 10
+p solution(8)
+p '-' * 10
+p solution(9)
+p '-' * 10
+p solution(32)
+p '-' * 10
+p solution(11)
+p '-' * 10
+p solution(19)
+p '-' * 10
+p solution(1376796946)
+p '-' * 10
+p solution(74901729)
