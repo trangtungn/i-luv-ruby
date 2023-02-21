addc703c32d187403fc5b22f0855e89565d23f96
Trang Tung Nguyen <trangtungn@gmail.com>
2023-02-21 12:49:47 +0700

    Optimize max counters

--- dsa_challenges/colidity/max_counters.rb
+++ dsa_challenges/colidity/max_counters.rb
@@ -4,18 +4,45 @@
 def solution(n, a)
   counters = Array.new(n, 0)
 
-  limit = n + 1
+  maximized = false
   max_counter = 0
-  a.each_with_index do |val, idx|
-    next if val > limit
-
+  a.each do |val|
     if val >= 1 && val <= n
+      maximized = false
+
       counters[val - 1] += 1
       max_counter = counters[val - 1] if max_counter < counters[val - 1]
+    elsif val == n + 1
+      counters = Array.new(n, max_counter) unless maximized
+      maximized = true
     end
-
-    counters = Array.new(n, max_counter) if val == limit
   end
 
   counters
 end
+
+def solution2(n, a)
+  counters = Array.new(n, 0)
+  max_counter = 0
+  min_counter = 0
+
+  a.each do |val|
+    if val >= 1 && val <= n
+      counters[val - 1] = [counters[val - 1], min_counter].max + 1
+      max_counter = [max_counter, counters[val - 1]].max
+    elsif val == n + 1
+      min_counter = max_counter
+    end
+    p counters
+    p '-'
+  end
+
+  counters.map { |count| [count, min_counter].max }
+end
+
+p solution(5, [3, 4, 4, 6, 1, 4, 4])
+p solution2(5, [3, 4, 4, 6, 1, 4, 4])
+p '---'
+
+p solution(5, [3, 6, 4, 6, 1, 4])
+p solution2(5, [3, 6, 4, 6, 1, 4])
