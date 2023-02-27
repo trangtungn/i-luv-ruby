d3752ee9ed790b3bad2e86f134e0ffecc335d6d6
Trang Tung Nguyen <trangtungn@gmail.com>
2023-02-27 14:44:59 +0700

    add solution to task three

--- dsa_challenges/colidity/challenges/task_three.rb
+++ dsa_challenges/colidity/challenges/task_three.rb
@@ -1,7 +1,39 @@
 #!/Users/trangtungn/.rbenv/shims/ruby -w
 # frozen_string_literal: true
 
+require 'set'
+
 def solution(connections, name1, name2)
+  graph = Hash.new { |h, k| h[k] = [] }
+
+  # Build the graph
+  connections.each do |connection|
+    name_a, name_b = connection.split(":")
+    graph[name_a] << name_b
+    graph[name_b] << name_a
+  end
+
+  # Perform a breadth-first search to find the shortest path between the two names
+  queue = [[name1, 0]]
+  visited = Set.new([name1])
+
+  while !queue.empty?
+    name, distance = queue.shift
+    return distance if name == name2
+
+    graph[name].each do |friend|
+      next if visited.include?(friend)
+
+      visited << friend
+      queue << [friend, distance + 1]
+    end
+  end
+
+  # If we've searched the entire graph and haven't found a path to the target name, return -1
+  -1
+end
+
+def solution2(connections, name1, name2)
   return -1 if connections.empty?
 
   if connections.size == 1
@@ -14,8 +46,7 @@ def solution(connections, name1, name2)
   connections.each do |con|
     head, tail = con.split(':')
 
-    con_mapper[head] = [] if con_mapper[head].nil?
-    con_mapper[head] << tail
+    con_mapper[head] = tail
   end
 
   count = 0
@@ -27,35 +58,17 @@ def solution(connections, name1, name2)
 
     count += 1
 
-    last.each do |one, two|
-      if con_mapper[last] == name2
-        match = true
-        break
-      end
+    if con_mapper[last] == name2
+      match = true
+      break
     end
+
     first = last
   end
 
   return count if match == true
 
   -1
-end
-
-def find(conns, name1, name2)
-  return -1 if conns.empty?
-
-  last = con_mapper.delete(first)
-  return -1 if last.nil? || last.empty?
-
-  count += 1
-
-  last.each do |one, two|
-    find(conns, name1, name2)
-  end
-
-  return 1 if con_mapper[last] == name2
-
-  first = last
 end
 
 args = [
@@ -66,6 +79,7 @@ args = [
 ]
 
 args.each do |arg|
-  p solution(*arg)
-  p '-' * 10
+  p '-' * 20
+  p "#1: #{solution(*arg)}"
+  p "#2: #{solution2(*arg)}"
 end
