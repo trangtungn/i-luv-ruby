dbca810916e357cc3299089833fcd9fea39a88e2
Trang Tung Nguyen <trangtungn@gmail.com>
2024-11-28 16:16:58 +0700

    update single splat

--- ruby_splat/single_splat.rb
+++ ruby_splat/single_splat.rb
@@ -6,33 +6,33 @@ p <<~INTRO
   you would use a splat operator.
 INTRO
 
-arr = %w[first second third]
-
-# makes three arguments
-def group_array_args(*args)
-  p args # [["first", "second", "third"]]
-end
-
-p '1. Group params (array) to array'
-group_array_args(arr)
-
 one = 'first'
 two = 'second'
 three = 'third'
 
-p '2. Group params (n variable) to array'
-def group_variables(*args)
-  p args # ["first", "second", "third"]
+arr = %w[first second third]
+
+# makes three arguments
+def group_array_args(*args)
+  p args
 end
 
-group_variables(one, two, three)
+p '1. Group params (array) to array'
+group_array_args(arr) # [["first", "second", "third"]]
+
+p '2. Group params (n variable) to array'
+def group_variables(*args)
+  p args
+end
+
+group_variables(one, two, three) # ["first", "second", "third"]
 
 p '3. Splat array to array.length params'
 def array_to_args(one, two, three)
   p one, two, three
 end
 
-array_to_args(*arr)
+array_to_args(*arr) # first second third
 
 p '4. Grab array in to segments'
 def grab_array_segments
